d97336164504d26f04b1acf9c6960a73610e16a4
Imran Mohamed <info@imranbmohamed.com>
2023-07-17 10:31:24 +0100

    add cors

--- app.js
+++ app.js
@@ -1,10 +1,13 @@
 const express = require('express')
+const cors = require('cors');
 
 const { handleServerErrors, handlePsqlErrors, handleCustomErrors } = require('./errors/errors')
 
 const apiRouter = require("./routes/api-router");
 
 const app = express()
+
+app.use(cors())
 app.use(express.json())
 
 app.use("/api", apiRouter)
